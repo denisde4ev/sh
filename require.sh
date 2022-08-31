4100a538be74d3162f9d83acb9ead628509cd0e0
Denis Dechev (u@op5) <denisde4ev@vivaldi.net>
2022-08-31 12:24:39 +0300

    move prints and return outside eval

--- require.sh
+++ require.sh
@@ -5,7 +5,7 @@ case ${0##*/}:$1 in require.sh:--help|require:--help)
 		"  * files once sourced will not be sourced again" \
 		"  * will make sure vars does not exist exist before sourcing" \
 		"    and they do after sourcing, otherwise will give return status 5" \
-		"  * vars are separated by IFS var" \
+		"  * vars/fns are separated by IFS var" \
 		"" \
 		"  this script is only ment to be sourced by other shell scripts:" \
 		'  `set -eu; . '"$0"'; require '\''$var1'\'' ./define-var1.sh; echo "$var1";`' \
@@ -20,22 +20,40 @@ require() {
 		*) return 2;;
 	esac
 
+	local _require_tmp || :
+
 	case :$_require_sourced: in *:"$2":*) ;; *)
 		# set -f # dont: can not recover previous state in POSIX sh  (TODO not shure if its true)
-		for i in $1; do
-			case $i in # must not exitst in this shell
-				'$'*) eval "case \${${i#\$}+x} in x) printf %s\\\\n >&2 'require: var ${i#\$} is defined before require' ; return 5; esac";;
-				*) case $(command -v "$i") in */*|'') ;; *)  printf %s\\\\n >&2 'require: fn $i is defined before require'; return 5; esac;;
+		for _require_tmp in $1; do
+			case $_require_tmp in # vars/fns must NOT exitst
+				'$'*)
+					eval "case \${${_require_tmp#\$}+x} in '') continue; esac"
+					printf %s\\n >&2 'require: var ${_require_tmp#\$} is defined before require'
+					unset _require_tmp; return 5
+				;;
+				*)
+					case $(command -v "$_require_tmp") in */*|'') continue; esac
+					printf %s\\n >&2 'require: fn $_require_tmp is defined before require'
+					unset _require_tmp; return 5
+				;;
 			esac
 		done
 		_require_sourced=$_require_sourced:$2
-		. "$2"
+		. "$2" || return
 	esac
 
-	for i in $1; do
-		case $i in # must exitst in this shell
-			'$'*) eval "case \${${i#\$}+x} in '') printf %s\\\\n >&2 'require: var ${i#\$} is not defined after require' ; return 5; esac";;
-			*) case $(command -v "$i") in ''|*/*) printf %s\\\\n >&2 'require: fn $i is not defined after require'; return 5; esac;;
+	for _require_tmp in $1; do
+		case $_require_tmp in # vars/fns must exitst
+			'$'*)
+				eval "case \${${_require_tmp#\$}+x} in x) continue; esac"
+				printf %s\\n >&2 'require: var ${_require_tmp#\$} is not defined after require'
+				unset _require_tmp; return 5
+			;;
+			*)
+				case $(command -v "$_require_tmp") in */*|'') ;; *) continue; esac
+				printf %s\\n >&2 'require: fn $_require_tmp is not defined after require'
+				unset _require_tmp; return 5
+			;;
 		esac
 	done
 }
