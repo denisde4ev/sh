096903632c3278c9f05394275f13ba532001fa5f
Denis Dechev (u@op5) <denisde4ev@vivaldi.net>
2022-08-31 12:26:03 +0300

    add self require if no commands in PATH; fix: "\'" -> "\""

--- require.sh
+++ require.sh
@@ -1,17 +1,28 @@
 
 case ${0##*/}:$1 in require.sh:--help|require:--help)
-	printf %s\\n \
-		"usage: require"' <"$var1 $var2 fn1 fn2"> [=] <source-file>' \
-		"  * files once sourced will not be sourced again" \
-		"  * will make sure vars does not exist exist before sourcing" \
-		"    and they do after sourcing, otherwise will give return status 5" \
-		"  * vars/fns are separated by IFS var" \
-		"" \
-		"  this script is only ment to be sourced by other shell scripts:" \
-		'  `set -eu; . '"$0"'; require '\''$var1'\'' ./define-var1.sh; echo "$var1";`' \
-	;
-	exit
+command -v cat 2>/dev/null 1>&2 || {  set --; . "$0"; require cat lib:cat;  }
+cat << EOF
+usage: require <"\$var1 \$var2 fn1 fn2"> [=] <source-file>
+* files once sourced will not be sourced again
+* will make sure vars does not exist exist before sourcing
+  and they do after sourcing, otherwise will give return status 5
+* vars/fns are separated by IFS var
+* when source-file is in lib:* then will source inside of dir \$require_libpath
+
+this script is ment to be sourced by other shell scripts:
+\`set -eu; . '$0'; require '\$var1' ./define-var1.sh; echo "\$var1"\`
+EOF
+exit
 esac
+
+
+: "${require_libpath:=/^/ https://github.com/denisde4ev/sh/tree/master}"
+
+case $SYSTEMDRIVE in [A-Z]:)
+	# try (requires on ash/bash pattern replacement) to fix windows path
+	eval 'require_libpath=${SYSTEMDRIVE-}'"\${require_libpath//https:\/\//}" || :
+esac
+
 
 require() {
 	case $#:$2 in
@@ -19,6 +30,7 @@ require() {
 		2:*) ;;
 		*) return 2;;
 	esac
+	case $1 in lib:*) set -- "$require_libpath/${1#lib:}" "$2"; esac
 
 	local _require_tmp || :
 
@@ -28,12 +40,12 @@ require() {
 			case $_require_tmp in # vars/fns must NOT exitst
 				'$'*)
 					eval "case \${${_require_tmp#\$}+x} in '') continue; esac"
-					printf %s\\n >&2 'require: var ${_require_tmp#\$} is defined before require'
+					printf %s\\n >&2 "require: var ${_require_tmp#\$} is defined before require"
 					unset _require_tmp; return 5
 				;;
 				*)
 					case $(command -v "$_require_tmp") in */*|'') continue; esac
-					printf %s\\n >&2 'require: fn $_require_tmp is defined before require'
+					printf %s\\n >&2 "require: fn $_require_tmp is defined before require"
 					unset _require_tmp; return 5
 				;;
 			esac
@@ -46,14 +58,18 @@ require() {
 		case $_require_tmp in # vars/fns must exitst
 			'$'*)
 				eval "case \${${_require_tmp#\$}+x} in x) continue; esac"
-				printf %s\\n >&2 'require: var ${_require_tmp#\$} is not defined after require'
+				printf %s\\n >&2 "require: var ${_require_tmp#\$} is not defined after require"
 				unset _require_tmp; return 5
 			;;
 			*)
 				case $(command -v "$_require_tmp") in */*|'') ;; *) continue; esac
-				printf %s\\n >&2 'require: fn $_require_tmp is not defined after require'
+				printf %s\\n >&2 "require: fn $_require_tmp is not defined after require"
 				unset _require_tmp; return 5
 			;;
 		esac
 	done
 }
+
+case $(command -v local) in local) ;; *)
+	require local lib:local;;
+esac
