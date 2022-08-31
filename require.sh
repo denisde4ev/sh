103019b271aa93cdc30bc52adac6d7eb47997c85
Denis Dechev (u@op5) <denisde4ev@vivaldi.net>
2022-08-31 12:22:55 +0300

    add error message

--- require.sh
+++ require.sh
@@ -7,7 +7,7 @@ case ${0##*/}:$1 in require.sh:--help|require:--help)
 		"    and they do after sourcing, otherwise will give return status 5" \
 		"  * vars are separated by IFS var" \
 		"" \
-		"  this script is only ment to be used as sourced by other shell scripts:" \
+		"  this script is only ment to be sourced by other shell scripts:" \
 		'  `set -eu; . '"$0"'; require '\''$var1'\'' ./define-var1.sh; echo "$var1";`' \
 	;
 	exit
@@ -20,22 +20,22 @@ require() {
 		*) return 2;;
 	esac
 
-	case :$__require_sourced: in *:"$2":*) ;; *)
+	case :$_require_sourced: in *:"$2":*) ;; *)
 		# set -f # dont: can not recover previous state in POSIX sh  (TODO not shure if its true)
 		for i in $1; do
 			case $i in # must not exitst in this shell
-				'$'*) eval "case \${${i#\$}+x} in x) return 5; esac";;
-				*) case $(command -v "$i") in */*|'') ;; *) return 5; esac;;
+				'$'*) eval "case \${${i#\$}+x} in x) printf %s\\\\n >&2 'require: var ${i#\$} is defined before require' ; return 5; esac";;
+				*) case $(command -v "$i") in */*|'') ;; *)  printf %s\\\\n >&2 'require: fn $i is defined before require'; return 5; esac;;
 			esac
 		done
-		__require_sourced=$__require_sourced:$2
+		_require_sourced=$_require_sourced:$2
 		. "$2"
 	esac
 
 	for i in $1; do
 		case $i in # must exitst in this shell
-			'$'*) eval "case \${${i#\$}+x} in '') return 5; esac";;
-			*) case $(command -v "$i") in ''|*/*) return 5; esac;;
+			'$'*) eval "case \${${i#\$}+x} in '') printf %s\\\\n >&2 'require: var ${i#\$} is not defined after require' ; return 5; esac";;
+			*) case $(command -v "$i") in ''|*/*) printf %s\\\\n >&2 'require: fn $i is not defined after require'; return 5; esac;;
 		esac
 	done
 }
